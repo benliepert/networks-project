ca74970d57bfa0977f3d333b93004ab97bdd6a2e
gezimsaciri <gezimsaciri@gmail.com>
2018-04-30 20:10:34 -0400

    more setup with Ben

--- client.c
+++ client.c
@@ -1,2 +1,18 @@
 // custom IRC client
 // created by Gezim Saciri, Ben Leipert, Josh Blaz
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+establish socket connection
+bind to the socket
+listen to the socket
+establish tcp connection
+send messages
+create a case for ending the connection
+*/
--- server.c
+++ server.c
@@ -1,2 +1,17 @@
 // custom IRC server
 // created by Gezim Saciri, Ben Leipert, Josh Blaz
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+establish socket connection
+bind to the socket
+listen to the socket
+master file descriptor
+wait for messages
+*/
