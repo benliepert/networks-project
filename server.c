9ae291590f310b4bcfaed748902fc3512e2880cb
Ben Liepert <lieper_b1@denison.edu>
2018-05-01 09:46:01 -0400

    added bind in create_connection()

--- server.c
+++ server.c
@@ -24,30 +24,44 @@ int create_connection()
     // create a struct for our address info
     struct addrinfo hints, *res;
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+    hints.ai_family = AF_UNSPEC;     //IPv4 or v6 is ok
+    hints.ai_socktype = SOCK_STREAM; //TCP stream sockets
+    hints.ai_flags = AI_PASSIVE;     //fill in my IP for me
 
     // built in struct to get our socket info
-    getaddrinfo("mail.denison.edu", "540001", &hints, &res);
+    int status;
+    if ((status = getaddrinfo("mail.denison.edu", "540001", &hints, &res)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    // === DO EVERYTHING BELOW ======================================================
 
     // make a socket
-    int s = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (s == -1)
-        printf("Socket Error %i\n", s);
+    int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sockfd == -1)
+        printf("Socket Error %i\n", sockfd);
+
+    // bind it to the port we passed in to getaddrinfo():
+    bind(sockfd, res->ai_addr, res->ai_addrlen);
 
     // connect
-	int connectStatus = connect(s, res->ai_addr, res->ai_addrlen);
-	if(connectStatus == -1)
-		printf("Connect Error %i\n", connectStatus);
-	
-	// return the socket for later calls
-	recvAll(s);
+    int connectStatus = connect(sockfd, res->ai_addr, res->ai_addrlen);
+    if (connectStatus == -1)
+        printf("Connect Error %i\n", connectStatus);
 
-	return s;
+    // return the socket for later calls
+    //recvAll(s);
+
+    //===============================================================================
+
+    freeaddrinfo(res); // free the linked list
+
+    return sockfd;
 }
 
 int main()
 {
-    socket = create_connection();
+    int socket = create_connection();
 }
